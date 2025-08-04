5bb37d1ac610635990fd0cb11d1585cecf11daca
kashitaka <makkausu@gmail.com>
2025-08-05 00:43:53 +0900

    leetcode: improve 70

--- leet-code/70_climbing_stairs/main.go
+++ leet-code/70_climbing_stairs/main.go
@@ -1,20 +1,11 @@
 package _0_climbing_stairs
 
 func climbStairs(n int) int {
-	if n == 1 {
-		return 1
+	dp := make([]int, n+1)
+	dp[0], dp[1] = 1, 1
+
+	for i := 2; i <= n; i++ {
+		dp[i] = dp[i-1] + dp[i-2]
 	}
-	if n == 2 {
-		return 2
-	}
-	cache := make(map[int]int)
-	cache[1] = 1
-	cache[2] = 2
-	for i:=3; i<=n; i++ {
-		// to reach step i, there are 2 ways
-		// - 2 steps up from i-2 stair
-		// - 1 step up from i-1 stair
-		cache[i] = cache[i-2] + cache[i-1]
-	}
-	return cache[n]
+	return dp[n]
 }
