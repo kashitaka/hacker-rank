7de749c1ab239b6666bf5821cd5b049c1fe6a613
kashitaka <makkausu@gmail.com>
2025-08-04 17:15:45 +0900

    leetcode: improve 1143

--- leet-code/1143_Longest_Common_Subsequence /main.go
+++ leet-code/1143_Longest_Common_Subsequence /main.go
@@ -1,6 +1,5 @@
 package _143_Longest_Common_Subsequence_
 
-
 // これはむずい！！
 // DPの奥深さ！
 func longestCommonSubsequence(text1 string, text2 string) int {
@@ -9,9 +8,9 @@ func longestCommonSubsequence(text1 string, text2 string) int {
 		dp[i] = make([]int, len(text2)+1)
 	}
 
-	for i := len(text1)-1; i >= 0; i-- {
+	for i := len(text1) - 1; i >= 0; i-- {
 		letter1 := text1[i]
-		for j := len(text2)-1; j >= 0; j-- {
+		for j := len(text2) - 1; j >= 0; j-- {
 			letter2 := text2[j]
 			if letter1 != letter2 {
 				dp[i][j] = max(dp[i+1][j], dp[i][j+1])
@@ -23,9 +22,19 @@ func longestCommonSubsequence(text1 string, text2 string) int {
 	return dp[0][0]
 }
 
-func max(i, j int) int {
-	if i > j {
-		return i
+func longestCommonSubsequence_memoryOptimized(text1 string, text2 string) int {
+	dp := make([]int, len(text2)+1)
+
+	for i := len(text1) - 1; i >= 0; i-- {
+		newDP := make([]int, len(text2)+1)
+		for j := len(text2) - 1; j >= 0; j-- {
+			if text1[i] == text2[j] {
+				newDP[j] = 1 + dp[j+1]
+			} else {
+				newDP[j] = max(dp[j], newDP[j+1])
+			}
+		}
+		dp = newDP
 	}
-	return j
+	return dp[0]
 }
