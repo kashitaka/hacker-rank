b8ef5cfb456f5d412071f42d67622b67b88bbb5c
kashitaka <makkausu@gmail.com>
2025-08-05 00:07:35 +0900

    leetcode: improve 207

--- leet-code/207_course_schedule/main.go
+++ leet-code/207_course_schedule/main.go
@@ -1,42 +1,32 @@
 package _07_course_schedule
 
 func canFinish(numCourses int, prerequisites [][]int) bool {
-	preMap := make(map[int][]int)
-	for i := 0; i < numCourses; i++ {
-		preMap[i] = []int{}
+	dep := make(map[int][]int)
+	for _, v := range prerequisites {
+		dep[v[0]] = append(dep[v[0]], v[1])
 	}
+	visit := make(map[int]bool)
 
-	for _, req := range prerequisites {
-		preMap[req[0]] = append(preMap[req[0]], req[1])
-	}
-
-	visited := make(map[int]bool)
-
-	var dfs func(crs int) bool
-	dfs = func(crs int) bool {
-		if visited[crs] {
+	var dfs func(i int) bool
+	dfs = func(i int) bool {
+		if visit[i] {
 			return false
 		}
-		if len(preMap[crs]) == 0 {
+		next, ok := dep[i]
+		if !ok || len(next) == 0 {
 			return true
 		}
-		// ここの順番が大事。
-		// len(preMap[crs]) == 0 って訪れとるやん
-		// だが、preRequisitがないnodeは何度きてもいいので visitedフラグ立てない
-		visited[crs] = true
-
-		for _, nextCrs := range preMap[crs] {
-			if !dfs(nextCrs) {
+		visit[i] = true
+		for _, v := range next {
+			if !dfs(v) {
 				return false
 			}
 		}
-		// ここまできたってことはこのcrsは履修可能。
-		visited[crs] = false  // ここでバックトラッキング的にフラグ寝かさないと再訪できない
-		preMap[crs] = []int{} // 初期化
+		dep[i] = []int{}
+		visit[i] = false
 		return true
 	}
-
-	for k, _ := range preMap {
+	for k, _ := range dep {
 		if !dfs(k) {
 			return false
 		}
