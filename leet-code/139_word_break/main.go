9b9920a42bd35b4756fd24f37f4e4402e00a483b
kashitaka <makkausu@gmail.com>
2025-08-06 15:09:47 +0900

    leetcode: improve? 139

--- leet-code/139_word_break/main.go
+++ leet-code/139_word_break/main.go
@@ -7,36 +7,22 @@ package _39_word_break
 // dp[i] = dp[i - len(word)]
 // dp[8] = dp[8 - len("code")] = dp[8-4] = dp[4] = true
 
-func wordBreakBackword(s string, wordDict []string) bool {
+func wordBreak(s string, wordDict []string) bool {
+	// dp[i] shows if the substring[i:] can break with wordDict
 	dp := make([]bool, len(s)+1)
 	dp[len(s)] = true
 
 	for i := len(s) - 1; i >= 0; i-- {
 		for _, word := range wordDict {
-			if len(s)-i >= len(word) && s[i:len(word)+i] == word {
-				dp[i] = dp[i+len(word)]
-				if dp[i] {
-					break
-				}
+			wordLen := len(word)
+			if i+wordLen > len(s) {
+				continue
+			}
+			if word == s[i:i+wordLen] && dp[i+wordLen] {
+				dp[i] = true
+				break
 			}
 		}
 	}
 	return dp[0]
-}
-
-func wordBreakForward(s string, wordDict []string) bool {
-	dp := make([]bool, len(s)+1)
-	dp[0] = true
-
-	for i := 1; i <= len(s); i++ {
-		for _, word := range wordDict {
-			if i-len(word) >= 0 && s[i-len(word):i] == word {
-				dp[i] = dp[i-len(word)]
-				if dp[i] {
-					break
-				}
-			}
-		}
-	}
-	return dp[len(s)]
 }
