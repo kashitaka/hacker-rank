2c7af7f71ba18f50d5f3fae5008a30cc42888794
kashitaka <makkausu@gmail.com>
2025-08-15 15:16:17 +0900

    leetcode: add 981

--- leet-code/981_time_based_kvs/main.go
+++ leet-code/981_time_based_kvs/main.go
@@ -1,1 +1,58 @@
 package _81_time_based_kvs
+
+type Item struct {
+	ts  int
+	val string
+}
+
+type TimeMap struct {
+	hash map[string][]*Item
+}
+
+func Constructor() TimeMap {
+	return TimeMap{
+		hash: make(map[string][]*Item),
+	}
+}
+
+func (t *TimeMap) Set(key string, value string, timestamp int) {
+	if _, ok := t.hash[key]; !ok {
+		t.hash[key] = []*Item{}
+	}
+	t.hash[key] = append(t.hash[key], &Item{
+		ts: timestamp, val: value,
+	})
+}
+
+func (t *TimeMap) Get(key string, timestamp int) string {
+	list, ok := t.hash[key]
+	if !ok {
+		return ""
+	}
+	l, r := 0, len(list)-1
+	if timestamp < list[l].ts {
+		return ""
+	}
+	if list[r].ts <= timestamp {
+		return list[r].val
+	}
+
+	for l+1 < r {
+		mid := (l + r) / 2
+		if timestamp < list[mid].ts {
+			// search left
+			r = mid
+		} else {
+			// search right
+			l = mid
+		}
+	}
+	return list[l].val
+}
+
+/**
+ * Your TimeMap object will be instantiated and called as such:
+ * obj := Constructor();
+ * obj.Set(key,value,timestamp);
+ * param_2 := obj.Get(key,timestamp);
+ */
