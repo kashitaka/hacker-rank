c4f59fc183518122663426e401f9ca152cf8d83c
kashitaka <makkausu@gmail.com>
2025-08-18 14:02:49 +0900

    leetcode: add 287

---
+++ leet-code/287_find_the_duplicate_number/main.go
@@ -0,0 +1,21 @@
+package _87_find_the_duplicate_number
+
+func findDuplicate(nums []int) int {
+	slow, fast := 0, 0
+	for {
+		slow = nums[slow]
+		fast = nums[nums[fast]]
+		if slow == fast {
+			break
+		}
+	}
+
+	slow2 := 0
+	for {
+		slow = nums[slow]
+		slow2 = nums[slow2]
+		if slow == slow2 {
+			return slow
+		}
+	}
+}
