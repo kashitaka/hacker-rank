d45b6c164062b199bec3d0ba148139d2b3222a88
kashitaka <makkausu@gmail.com>
2025-08-21 15:49:54 +0900

    leetcode: mod 79

--- leet-code/79_word_search/main.go
+++ leet-code/79_word_search/main.go
@@ -1,40 +1,35 @@
 package _9_word_search
 
 func exist(board [][]byte, word string) bool {
-	ROW := len(board)
-	COL := len(board[0])
+	ROW, COL := len(board), len(board[0])
 	visit := make([][]bool, ROW)
 	for i, _ := range visit {
 		visit[i] = make([]bool, COL)
 	}
 
-	res := false
-	var dfs func(int, int, int)
-	dfs = func(row, col, idx int) {
-		target := word[idx]
-		if row < 0 || row == ROW || col < 0 || col == COL ||
-			visit[row][col] || target != board[row][col] {
-			return
+	var dfs func(row, col, idx int) bool
+	dfs = func(row, col, idx int) bool {
+		if row < 0 || col < 0 || row == ROW || col == COL ||
+			visit[row][col] || word[idx] != board[row][col] {
+			// this is wrong path
+			return false
 		}
 		if idx == len(word)-1 {
-			// found!!!
-			res = true
-			return
+			// found!!
+			return true
 		}
+		// this maybe right path. mark this cell visited and keep searching adjuscent cells
 		visit[row][col] = true
-		// find next
-		dfs(row-1, col, idx+1)
-		dfs(row+1, col, idx+1)
-		dfs(row, col-1, idx+1)
-		dfs(row, col+1, idx+1)
-		// backtracking
+		res := dfs(row-1, col, idx+1) || dfs(row+1, col, idx+1) ||
+			dfs(row, col-1, idx+1) || dfs(row, col+1, idx+1)
+		// backtrack
 		visit[row][col] = false
+		return res
 	}
 
-	for r, rows := range board {
-		for c, _ := range rows {
-			dfs(r, c, 0)
-			if res {
+	for r, row := range board {
+		for c, _ := range row {
+			if dfs(r, c, 0) {
 				return true
 			}
 		}
